2f51529898375be8298329f0f23a699ca8e3b3c1
JULIO SERGIO SOUSA DE MARIA <JULIO0697@DIGITALCOLLEGE.LOCAL>
2024-06-26 13:12:10 -0300

    envaindo arquivos

---
+++ switcase.js
@@ -0,0 +1,23 @@
+let escolha = 3 // valores 1 e 3
+
+
+
+switch (escolha){
+    case 1:
+        console.log('escolheu 1')
+
+    break;
+     case 2:
+        console.log('escolheu 2')
+
+    break;
+
+    case 3:
+        console.log('escolheu 3')
+
+    break;
+
+    default:
+        console.log ('Condição padrão.Atende qualquer escolha que nao seja 1,2,ou 3')
+        break;
+}
